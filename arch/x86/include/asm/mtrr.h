a3977a0de88f03e46d9271fecbab1aa319c997f5
Nitin A Kamble <nitin.a.kamble@intel.com>
2012-02-28 13:36:59 -0800

    Revert "Check __LP64__ instead of __i386__ for mtrr_gentry"
    This reverts commit 9dbff73eea2affb00db6afae347326c2cb49f917.

--- arch/x86/include/asm/mtrr.h
+++ arch/x86/include/asm/mtrr.h
@@ -40,7 +40,7 @@ struct mtrr_sentry {
    But you need to use this for 64bit, otherwise your X server
    will break. */
 
-#ifndef __LP64__
+#ifdef __i386__
 struct mtrr_gentry {
     unsigned int regnum;   /*  Register number  */
     unsigned long base;    /*  Base address     */
@@ -48,7 +48,7 @@ struct mtrr_gentry {
     unsigned int type;     /*  Type of region   */
 };
 
-#else /* !__LP64__ */
+#else /* __i386__ */
 
 struct mtrr_gentry {
     unsigned long base;    /*  Base address     */
@@ -56,7 +56,7 @@ struct mtrr_gentry {
     unsigned int regnum;   /*  Register number  */
     unsigned int type;     /*  Type of region   */
 };
-#endif /* __LP64__ */
+#endif /* !__i386__ */
 
 struct mtrr_var_range {
 	__u32 base_lo;
